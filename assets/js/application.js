e4a715cc643c20c738f33887fbf8d9a1353b80c9
Simon Mercier <contact@symerweb.com>
2014-12-29 02:40:18 -0500

    conflict

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,8 +3,5 @@ $(function(){
 });
 window.App = Ember.Application.create({
 	LOG_ACTIVE_GENERATION: true
-});
-
-io.socket.on('sendMessage', function(message){
-	$('.messages').append(message + '<br>');
+<<<<<<< HEAD
 });

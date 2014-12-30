e3b6b86d0a149e486cf5399945926db25833009d
Simon Mercier <contact@symerweb.com>
2014-12-30 05:50:55 -0500

    Persistance des messages lors du changement de vue

---
+++ assets/js/app/views/ChatsView.js
@@ -0,0 +1,8 @@
+App.ChatsView = Ember.View.extend({
+	templateName: 'chats',
+	didInsertElement: function(){
+		for (var i = 0; i < this.controller.messages.length; i++) {
+			$('.spc-messages').append(this.controller.messages[i]+'<br>');
+		};
+	}
+});

61e6f93ea15a2989d7e2359882a753b4fe263bc5
Shadowere <bobby_mousseau@hotmail.com>
2015-01-15 19:42:01 -0500

    Update style

--- assets/js/app/routes/ChatRoute.js
+++ assets/js/app/routes/ChatRoute.js
@@ -1,9 +1,10 @@
 App.ChatsRoute = Ember.Route.extend({
 	activate : function(){
 		var that = this;
-		io.socket.on('sendMessage', function(message){
-			$('.spc-messages').append(message + '</br>');
-			that.controller.messages.pushObject(message);
+		io.socket.on('sendMessage', function(data){
+			$('.spc-messages').append("<span class='chat-nickname'>" + data.nickname + "</span>:");
+			$('.spc-messages').append("<span class='chat-message'>" + data.message + "</span><br/>");
+			that.controller.messages.pushObject(data);
 		});
 	},
 

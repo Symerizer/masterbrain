0d4f870522060cf2cc69e5dcaf6edbce80db44df
Shadowere <bobby_mousseau@hotmail.com>
2015-01-17 16:37:20 -0500

    Modification scrollbar ajout limites (arros)

--- assets/js/app/views/ChatsView.js
+++ assets/js/app/views/ChatsView.js
@@ -2,7 +2,7 @@ App.ChatsView = Ember.View.extend({
 	templateName: 'chats',
 	didInsertElement: function(){
 		$('.chatInput').focus();
-		$('.spc-messages').jScrollPane();
+		$('.spc-messages').jScrollPane({showArrows:true});
 		var spcmessage = $('.spc-messages');
 		var jsp = spcmessage.data('jsp');
 		var atBottom = jsp.getPercentScrolledY() == 1;

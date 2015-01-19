ae33808b93724e2df3f9300fb48cddd9aae97ae8
acherist <shawndavidmerrifield@gmail.com>
2015-01-19 10:48:19 -0500

    test

--- assets/js/app/views/ChatsView.js
+++ assets/js/app/views/ChatsView.js
@@ -3,7 +3,8 @@ App.ChatsView = Ember.View.extend({
 	didInsertElement: function(){
 		$('.chatInput').focus();
 		for (var i = 0; i < this.controller.messages.length; i++) {
-			$('.spc-messages').append(this.controller.messages[i]);
+			$('.spc-messages').append("<span class='chat-nickname'>" + this.controller.messages[i].nickname + "</span>:&nbsp;");
+			$('.spc-messages').append("<span class='chat-message'>" + this.controller.messages[i].message + "</span><br/>");
 		};
 	}
 });
